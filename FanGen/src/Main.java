efc7eac8e733eb61c5300bbdb1cf1b5eefc29823
trime21 <36112611+trime21@users.noreply.github.com>
2018-09-03 23:33:50 +0200

    Update Main.java

--- FanGen/src/Main.java
+++ FanGen/src/Main.java
@@ -3,22 +3,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Fan fanGen = new Fan();
-        int size;
-        while ((size = input.nextInt()) != 0){
-            boolean direction = size > 0;
-            size = Math.abs(size)*2;
-            char tab[][][] = new char[4][size][size];
-            fanGen.makeFan(direction, size, tab);
-            fanGen.drawFan(size, fanGen.getRotated(tab, size));
-        }
+        new Fan().program();
     }
 }
 
 class Fan {
+    void program() {
+        Scanner input = new Scanner(System.in);
+        int size;
+        while ((size = input.nextInt()) != 0) {
+            new Draw().drawFan(new Rotated().getRotated(new Make().makeFan(new Direction().getDirection(size), new Table().getTable(new SizeArray().getSizeArray(size)))));
+        }
+    }
+}
 
-    boolean getCharRight(int line, int column){
+class SizeArray {
+    int getSizeArray(int size){
+        size = Math.abs(size) * 2;
+        return size;
+    }
+}
+
+class Table {
+    char[][][] getTable(int size){
+        char tab[][][] = new char[4][size][size];
+        return tab;
+    }
+}
+
+class RightChar {
+    boolean getCharRight(int line, int column) {
         boolean charTest = true;
         if (line >= column)
             charTest = true;
@@ -26,8 +40,10 @@ class Fan {
             charTest = false;
         return charTest;
     }
+}
 
-    boolean getCharLeft(int line, int column){
+class LeftChar {
+    boolean getCharLeft(int line, int column) {
         boolean charTest = true;
         if (column >= line)
             charTest = true;
@@ -35,8 +51,10 @@ class Fan {
             charTest = false;
         return charTest;
     }
+}
 
-    char getChar(boolean charTest){
+class Char{
+    char getChar(boolean charTest) {
         char charUsed;
         if (charTest)
             charUsed = '*';
@@ -44,47 +62,71 @@ class Fan {
             charUsed = '.';
         return charUsed;
     }
+}
 
-    public void makeFan(boolean direction, int size, char[][][] tab){
-        if (direction) {
-            for (int i = 0; i < size / 2; i++) {
-                for (int j = 0; j < size / 2; j++) {
-                    tab[0][i][j] = getChar(getCharRight(i, j));
-                }
-            }
-        } else {
-            for (int i = 0; i < size / 2; i++) {
-                for (int j = 0; j < size / 2; j++) {
-                    tab[0][i][j] = getChar(getCharLeft(i, j));
-                }
+class Direction {
+    boolean getDirection(int size) {
+        boolean direction = size > 0;
+        return direction;
+    }
+}
+
+class MakeRight {
+    public void makeRightFan(char[][][] tab) {
+        for (int i = 0; i < tab[0].length / 2; i++) {
+            for (int j = 0; j < tab[0][0].length / 2; j++) {
+                tab[0][i][j] = new Char().getChar(new RightChar().getCharRight(i, j));
             }
         }
-        getRotated(tab, size);
     }
+}
 
-    char[][][] getRotated(char tab[][][], int size){
+class MakeLeft {
+    public void makeLeftFan(char[][][] tab) {
+        for (int i = 0; i < tab[0].length / 2; i++) {
+            for (int j = 0; j < tab[0][0].length / 2; j++) {
+                tab[0][i][j] = new Char().getChar(new LeftChar().getCharLeft(i, j));
+            }
+        }
+    }
+}
+
+class Make {
+    char[][][] makeFan(boolean direction, char[][][] tab) {
+        if (direction)
+            new MakeRight().makeRightFan(tab);
+        else
+            new MakeLeft().makeLeftFan(tab);
+        return tab;
+    }
+}
+
+class Rotated {
+    char[][][] getRotated(char tab[][][]) {
         for (int r = 0; r < 3; r++) {
-            for (int i = 0; i < size / 2; i++) {
-                for (int j = 0; j < size / 2; j++) {
-                    tab[r+1][i][size / 2 - j - 1] = tab[r][j][i];
+            for (int i = 0; i < tab[0].length / 2; i++) {
+                for (int j = 0; j < tab[0][0].length / 2; j++) {
+                    tab[r + 1][i][tab[0][0].length / 2 - j - 1] = tab[r][j][i];
                 }
             }
         }
         return tab;
     }
+}
 
-    public void drawFan(int size, char[][][] tab) {
-        for (int i = 0; i < size / 2; i++) {
+class Draw {
+    public void drawFan(char[][][] tab) {
+        for (int i = 0; i < tab[0].length / 2; i++) {
             for (int r = 0; r < 2; r++) {
-                for (int j = 0; j < size / 2; j++) {
+                for (int j = 0; j < tab[0][0].length / 2; j++) {
                     System.out.print(tab[r][i][j]);
                 }
             }
             System.out.println();
         }
-        for (int i = 0; i < size / 2; i++) {
+        for (int i = 0; i < tab[0].length / 2; i++) {
             for (int r = 3; r > 1; r--) {
-                for (int j = 0; j < size / 2; j++) {
+                for (int j = 0; j < tab[0][0].length / 2; j++) {
                     System.out.print(tab[r][i][j]);
                 }
             }
