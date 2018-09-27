30e4f2e20d6457e5289374cc900f00633f756c7f
trime21 <36112611+trime21@users.noreply.github.com>
2018-09-27 14:47:14 +0200

    Update MakeRight.java

--- FanGen/src/MakeRight.java
+++ FanGen/src/MakeRight.java
@@ -2,9 +2,8 @@ public class MakeRight {
     
     public void makeRightFan(char[][][] tab) {
         for (int i = 0; i < tab[0].length / 2; i++) {
-            for (int j = 0; j < tab[0][0].length / 2; j++) {
+            for (int j = 0; j < tab[0][0].length / 2; j++)
                 tab[0][i][j] = new Char().getChar(new RightChar().getCharRight(i, j));
-            }
         }
     }
 }
