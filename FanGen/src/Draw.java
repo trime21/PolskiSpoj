299feda94a672f7d784b614fa58237a41208749d
trime21 <36112611+trime21@users.noreply.github.com>
2018-09-27 14:34:37 +0200

    Update Draw.java

--- FanGen/src/Draw.java
+++ FanGen/src/Draw.java
@@ -1,21 +1,21 @@
 public class Draw {
     public void drawFan(char[][][] tab) {
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < tab[0].length / 2; i++) {
             for (int r = 0; r < 2; r++) {
-                for (int j = 0; j < tab[0][0].length / 2; j++) {
-                    System.out.print(tab[r][i][j]);
-                }
+                for (int j = 0; j < tab[0][0].length / 2; j++)
+                    output.write(tab[r][i][j]);
             }
-            System.out.println();
+            output.newLine();
         }
         for (int i = 0; i < tab[0].length / 2; i++) {
             for (int r = 3; r > 1; r--) {
-                for (int j = 0; j < tab[0][0].length / 2; j++) {
-                    System.out.print(tab[r][i][j]);
-                }
+                for (int j = 0; j < tab[0][0].length / 2; j++)
+                    output.write(tab[r][i][j]);
             }
-            System.out.println();
+            output.newLine();
         }
-        System.out.println();
+        output.newLine();
+        output.flush();
     }
 }
