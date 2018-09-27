da4df13f66495ccc32f2a6f24037e21bba787f6c
trime21 <36112611+trime21@users.noreply.github.com>
2018-09-27 14:36:09 +0200

    Update Fan.java

--- FanGen/src/Fan.java
+++ FanGen/src/Fan.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Fan {
-    void program() {
-        Scanner input = new Scanner(System.in);
+    void program() throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int size;
-        while ((size = input.nextInt()) != 0) {
+        while ((size = Integer.parseInt(input.readLine())) != 0){
             new Draw().drawFan(new Rotated().getRotated(new Make().makeFan(new Direction().getDirection(size), new Array().getArray(new SizeArray().getSizeArray(size)))));
         }
     }
